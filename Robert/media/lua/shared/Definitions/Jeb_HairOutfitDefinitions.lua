973a04bab59b8f66f1db141629218c993a063a92
Mathieu M. Gélinas <mathieu.m.gelinas5@gmail.com>
2022-02-11 22:14:28 -0500

    Can spawn Robert from debug tool

--- Robert/media/lua/shared/Definitions/Jeb_HairOutfitDefinitions.lua
+++ Robert/media/lua/shared/Definitions/Jeb_HairOutfitDefinitions.lua
@@ -8,5 +8,5 @@ local cat = {};
 cat.outfit = "JebRobert";
 cat.haircut = "ShortAfroCurly:100";
 cat.beard = "Moustache:100";
-cat.haircutColor = "0.99,0.99,0.99:100";
+cat.haircutColor = "0.1,0.1,0.1:100";
 table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
