0999551d07ec21ba88b6349d59b313fdbd50a1db
Mathieu M. Gélinas <mathieu.m.gelinas5@gmail.com>
2022-02-11 20:43:55 -0500

    Multiple changes while debugging

--- Robert/media/scripts/Jeb_HairOutfitDefinitions.lua
+++ Robert/media/scripts/Jeb_HairOutfitDefinitions.lua
@@ -1,12 +1,12 @@
 require 'Definitions/HairOutfitDefinitions'
 
---Remove all OutfitDefinitions, even those coming from Zomboid itself.
-HairOutfitDefinitions.haircutOutfitDefinition = {}
+-- Remove all OutfitDefinitions, even those coming from Zomboid itself.
+-- HairOutfitDefinitions.haircutOutfitDefinition = {}
 
 --Adding the one, and only, true good zombie : Robert.
-local OutfitDefinition = {};
-OutfitDefinition.outfit = "JebRobert";
-OutfitDefinition.haircut = "ShortAfroCurly:100";
-OutfitDefinition.beard = "Moustache:100";
-OutfitDefinition.haircutColor = "0.99,0.99,0.99:100";
-table.insert(HairOutfitDefinitions.haircutOutfitDefinition, OutfitDefinition);
+local cat = {};
+cat.outfit = "JebRobert";
+cat.haircut = "ShortAfroCurly:100";
+cat.beard = "Moustache:100";
+cat.haircutColor = "0.99,0.99,0.99:100";
+table.insert(HairOutfitDefinitions.haircutOutfitDefinition, cat);
